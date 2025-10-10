99b2dca9f4308684fcc17fd59261141da3ce7097
jesus emmanuel luis sandoval <jemanuelluissandoval@gmail.com>
2025-10-10 14:54:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,12 @@
-//Crear servidor web
+// Create web server
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// middleware para analizar cuerpos JSON
-app.use(express.json());
-
-// definir rutas
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo!');
+  res.send('Hello World!');
 });
-app.post('/comments', (req, res) => {
-  const comment = req.body;
-  // TODO: Guardar comentario en la base de datos
-  res.status(201).send(comment);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
